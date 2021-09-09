d7828036e9d5d5b93b135a32be6c989bcfbc7a6c
Luiz Luchete <luizfernandolv@gmail.com>
2021-09-09 20:32:44 -0300

    feat: ensure DbCreateAccount calls CreateAccount with correct values

--- src/data/usecases/create-account/db-create-account.ts
+++ src/data/usecases/create-account/db-create-account.ts
@@ -1,9 +1,15 @@
-import { AccountModel, CreateAccount, CreateAccountDTO, Encrypter } from './db-create-account.protocols'
+import { AccountModel, CreateAccount, CreateAccountDTO, CreateAccountRepository, Encrypter } from './db-create-account.protocols'
 
 export class DbCreateAccount implements CreateAccount {
-  constructor (private readonly encrypter: Encrypter) {}
-  async create (account: CreateAccountDTO): Promise<AccountModel> {
-    await this.encrypter.encrypt(account.password)
+  constructor (
+    private readonly encrypter: Encrypter,
+    private readonly createAccountRepository: CreateAccountRepository) {}
+
+  async create (createAccount: CreateAccountDTO): Promise<AccountModel> {
+    const hashedPassword = await this.encrypter.encrypt(createAccount.password)
+    await this.createAccountRepository.create(Object.assign({}, createAccount, {
+      password: hashedPassword
+    }))
 
     return await new Promise(resolve => resolve({
       email: '',
