856bb497cfcdccbcc9431fa9318476cb45ddab24
Luiz Luchete <luizfernandolv@gmail.com>
2021-09-09 15:11:31 -0300

    test: ensure EmailValidatorAdapter retuns false if validator returns false

---
+++ src/utils/email-validator-adapter.spec.ts
@@ -0,0 +1,14 @@
+import { EmailValidatorAdapter } from './email-validator'
+describe('EmailValidator Adapter', () => {
+  test('Should return false if validator returns false', () => {
+    const sut = new EmailValidatorAdapter()
+    const isValid = sut.isValid('invalid_email@mail.com')
+    expect(isValid).toBe(false)
+  })
+
+  test('Should return true if validator returns true', () => {
+    const sut = new EmailValidatorAdapter()
+    const isValid = sut.isValid('invalid_email@mail.com')
+    expect(isValid).toBe(false)
+  })
+})
