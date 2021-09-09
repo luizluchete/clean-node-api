f12f11be7c1a223179542042e3b0fe857e6654c5
Luiz Luchete <luizfernandolv@gmail.com>
2021-09-09 14:19:21 -0300

    refactor: move sucess http response to a helper method

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -1,5 +1,5 @@
 import { MissingParamError, InvalidParamError } from '../../errors'
-import { badRequest, serverError, ok } from '../../helpers/http-helper'
+import { badRequest, serverError } from '../../helpers/http-helper'
 import { HttpRequest, HttpResponse, Controller, EmailValidator, CreateAccount } from './signup-protocols'
 
 export class SignUpController implements Controller {
@@ -33,8 +33,11 @@ export class SignUpController implements Controller {
         email,
         password
       })
+      return {
+        statusCode: 200,
+        body: account
 
-      return ok(account)
+      }
     } catch (error) {
       return serverError()
     }
