389f6da0b25e3f96bed1d95254d9d059abc4fca7
Luiz Luchete <luizfernandolv@gmail.com>
2021-09-11 20:52:09 -0300

    feat: ensure JwtAdapter returns a token when sign succeeds

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -1,5 +1,11 @@
 import jwt from 'jsonwebtoken'
 import { JwtAdapter } from './jwt-adapter'
+
+jest.mock('jsonwebtoken', () => ({
+  async sign (): Promise<string> {
+    return await new Promise(resolve => resolve('any_token'))
+  }
+}))
 describe('Jwt Adapter', () => {
   test('Should call sign with correct values', async () => {
     const sut = new JwtAdapter('secret')
@@ -7,5 +13,12 @@ describe('Jwt Adapter', () => {
     await sut.encrypt('any_id')
 
     expect(signSpy).toBeCalledWith({ id: 'any_id' }, 'secret')
+  })
+
+  test('Should return a token on sign succees', async () => {
+    const sut = new JwtAdapter('secret')
+    const acessToken = await sut.encrypt('any_id')
+
+    expect(acessToken).toBe('any_token')
   })
 })
--- src/infra/criptography/jwt-adapter/jwt-adapter.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.ts
@@ -4,7 +4,7 @@ import { Encrypter } from '../../../data/protocols/criptography/encrypter'
 export class JwtAdapter implements Encrypter {
   constructor (private readonly secret: string) {}
   async encrypt (value: string): Promise<string> {
-    jwt.sign({ id: value }, this.secret)
-    return null
+    const acessToken = jwt.sign({ id: value }, this.secret)
+    return acessToken
   }
 }
